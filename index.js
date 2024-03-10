0f1c6e2f4261c7b1ab631fda1acba64373a9c3af
EihabAhmed <eihabahmed@gmail.com>
2024-03-10 05:42:30 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+io.on("connection", (client) => {
+  console.log("connection received");
+  client.on("new_message", (score) => {
+    console.log(`new message received: ${score}`);
+    io.emit("broadcast", score);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`server running at ${port}...`);
+});
